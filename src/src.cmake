115c4c151b04f13c7c12ca2e1923a40353b09915
zhangjipeng <zhangjipeng@xiaomi.com>
2024-09-20 12:59:44 +0800

    fix win32 build

--- src/src.cmake
+++ src/src.cmake
@@ -66,8 +66,8 @@ include_directories(${PROJ_ROOT}/include
 set(LIB_NAME agave)
 
 if (MSVC)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /wd4838 /wd4305 /wd4291 /wd4065 /wd4146")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4838 /wd4305 /wd4291 /wd4065 /wd4146")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /wd4838 /wd4305 /wd4291 /wd4065 /wd4146 /wd4756")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4838 /wd4305 /wd4291 /wd4065 /wd4146 /wd4756")
 else()
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wmisleading-indentation")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wmisleading-indentation")
