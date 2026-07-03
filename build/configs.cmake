fc51b417e3468a1d2be6b1ba718f78d080ce07e4
onecoolx <onecoolx@gmail.com>
2026-07-03 17:22:42 +0800

    fix: fix windows build

--- build/configs.cmake
+++ build/configs.cmake
@@ -4,10 +4,10 @@
 # Contact: onecoolx@gmail.com
 
 if (WIN32)
-    set(CMAKE_C_FLAGS_DEBUG  "${CMAKE_C_FLAGS_DEBUG} /D_DEBUG /W3 /WX /Gm- /Gd /Zc:wchar_t- /MP")
-    set(CMAKE_CXX_FLAGS_DEBUG  "${CMAKE_CXX_FLAGS_DEBUG} /D_DEBUG /W3 /WX /Gm- /Gd /Zc:wchar_t- /MP")
-    set(CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS_RELEASE} /W3 /WX /Ot /GF /Gm- /Gy /GL /Zc:wchar_t- /MP")
-    set(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_CXX_FLAGS_RELEASE} /W3 /WX /Ot /GF /Gm- /Gy /GL /Zc:wchar_t- /MP")
+    set(CMAKE_C_FLAGS_DEBUG  "${CMAKE_C_FLAGS_DEBUG} /D_DEBUG /W3 /WX /Gm- /Gd /Zc:wchar_t- /MP /utf-8")
+    set(CMAKE_CXX_FLAGS_DEBUG  "${CMAKE_CXX_FLAGS_DEBUG} /D_DEBUG /W3 /WX /Gm- /Gd /Zc:wchar_t- /MP /utf-8")
+    set(CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS_RELEASE} /W3 /WX /Ot /GF /Gm- /Gy /GL /Zc:wchar_t- /MP /utf-8")
+    set(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_CXX_FLAGS_RELEASE} /W3 /WX /Ot /GF /Gm- /Gy /GL /Zc:wchar_t- /MP /utf-8")
     set(CMAKE_C_FLAGS_RELEASE ${CMAKE_C_FLAGS_MINSIZEREL})
     set(CMAKE_CXX_FLAGS_RELEASE ${CMAKE_CXX_FLAGS_MINSIZEREL})
     set(CMAKE_C_FLAGS_RELWITHDEBINFO ${CMAKE_C_FLAGS_MINSIZEREL})
